f618279c2360846f6cfd64070bace674c8627473
TANISHQ TIWARI <tanishq.tiwari2812@gmail.com>
2025-08-28 15:20:18 +0530

    UI updated

--- server/index.js
+++ server/index.js
@@ -1,33 +1,28 @@
-import express from 'express';
-import * as dotenv from 'dotenv';
-import cors from 'cors';
+import express from 'express'
+import * as dotenv from 'dotenv'
+import cors from 'cors'
 
-import connectDB from './mongodb/connect.js';
-import postRoutes from './routes/postRoutes.js';
-import dalleRoutes from './routes/dalleRoutes.js';
+import connectDB from './MongoDB/connect.js';
 
 dotenv.config();
 
 const app = express();
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
+app.use(express.json({limit : '60mb'}));
 
-app.use('/api/v1/post', postRoutes);
-app.use('/api/v1/dalle', dalleRoutes);
-
-app.get('/', async (req, res) => {
-  res.status(200).json({
-    message: 'Hello from DALL.E!',
-  });
-});
+app.get('/',async(req,res) => {
+    res.send('Hello from Image Generator')
+})
 
 const startServer = async () => {
-  try {
-    connectDB(process.env.MONGODB_URL);
-    app.listen(8080, () => console.log('Server started on port 8080'));
-  } catch (error) {
-    console.log(error);
-  }
-};
+    try {
+        connectDB(process.env.MONGODB_URL)
+        app.listen(9000 , () => {
+        console.log('Server has started on port http://localhost:9000')
+    })
+    } catch (error) {
+        console.log(error);
+    }
+}
 
-startServer();
+startServer()
--- server/mongodb/connect.js
+++ server/mongodb/connect.js
@@ -1,13 +1,16 @@
-import mongoose from 'mongoose';
+import mongoose from 'mongoose'
 
 const connectDB = (url) => {
-  mongoose.set('strictQuery', true);
-  mongoose.connect(url)
-    .then(() => console.log('connected to mongo'))
+    mongoose.set('strictQuery' , true)
+
+    mongoose.connect(url)
+    .then(() => {
+        console.log('Database connected')
+    })
     .catch((err) => {
-      console.error('failed to connect with mongo');
-      console.error(err);
-    });
-};
+        console.log(err);
+        
+    })
+}
 
 export default connectDB;
